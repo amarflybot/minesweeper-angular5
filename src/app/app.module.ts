e21db2cad37b385abc6f805347de697e49d8d1c1
amar.arawat@gmail.com <amar.arawat@gmail.com>
2018-03-25 10:47:27 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {AppNavbarComponent} from './app-navbar/app-navbar.component';
+import { HomeComponent } from './home/home.component';
+import {FormsModule} from '@angular/forms';
+import { ComputerComponent } from './computer/computer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    HomeComponent,
+    ComputerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
